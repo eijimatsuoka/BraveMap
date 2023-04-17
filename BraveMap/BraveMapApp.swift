e0b8f57eb953abafe43243dcae31bdaebc164d82
eijimatsuoka <t.jgreeeen550229@icloud.com>
2023-04-17 22:13:50 +0900

    Initial Commit

---
+++ BraveMap/BraveMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraveMapApp.swift
+//  BraveMap
+//
+//  Created by 松岡栄志 on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct BraveMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BraveMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BraveMap
+//
+//  Created by 松岡栄志 on 2023/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
